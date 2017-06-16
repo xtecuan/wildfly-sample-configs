95dc14936a0bfde43d1c93dccdbe4a0039a3825c
Tadeo Rivera <xtecuan@gmail.com>
2017-06-15 20:53:47 -0400

    My Wildfly development config Wildfly version: 10.1.0.Final

---
+++ my-standalone-full-for-dev/bin/stopStandaloneServer.bat
@@ -0,0 +1,6 @@
+@echo off
+set "JBOSS_HOME=C:\Work\Java\wildfly-10.1.0.Final"
+set "JAVA_HOME=C:\Work\Java\jdk1.8.0_131"
+set "PATH=%JAVA_HOME%\bin;%PATH%"
+
+%JBOSS_HOME%\bin\jboss-cli.bat --connect command=:shutdown
